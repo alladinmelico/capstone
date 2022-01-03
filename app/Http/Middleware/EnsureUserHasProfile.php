5b01e062f8cf7ae64a394b58aee405bd5e61c22f
Alladin Melico <alladin.melico@appetiser.com.au>
2022-01-03 11:40:55 +0800

    fix not contains

--- app/Http/Middleware/EnsureUserHasProfile.php
+++ app/Http/Middleware/EnsureUserHasProfile.php
@@ -19,7 +19,7 @@ class EnsureUserHasProfile
     {
         $user = Auth::user();
 
-        if (empty($user->school_id) && Auth::user() && str_contains(config('constants.admins'), Auth::user()->email)) {
+        if (empty($user->school_id) && Auth::user() && !str_contains(config('constants.admins'), Auth::user()->email)) {
             abort(419);
         }
         return $next($request);
