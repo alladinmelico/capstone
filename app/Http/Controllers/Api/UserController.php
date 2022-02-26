8028b6bddc9482e800f573cbb2a6314f9770421a
alladin.melico <alladin.melico@appetiser.com.au>
2022-02-27 02:40:29 +0800

    removed section clmn

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
                         $query->select('user_id')->from('classroom_users')->where('classroom_id', $request->classroom_id);
                     });
                 })
-                ->with(['course'])
+                ->with(['course', 'section'])
                 ->orderBy('updated_at', 'desc')
                 ->paginate($request->limit)
         );
